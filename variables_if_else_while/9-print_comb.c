09d316cf7726b33e9196c75c0238525abeaac44d
DANDEV2433 <alexdani3344@gmail.com>
2024-10-24 11:26:20 +0000

    putchar

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
  * main - Entry point
@@ -10,11 +8,11 @@ int main(void)
 {
 int i;
 
-for (i = '0'; i <= '9'; i++)
+for (i = 0; i <= 9; i++)
 {
 putchar(i + '0');
 
-if (i < '9')
+if (i < 9)
 {
 putchar(',');
 putchar(' ');
