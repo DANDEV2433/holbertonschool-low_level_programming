5a069e894349d8b1d8c04c58e89ac55c3bd00ec7
DANDEV2433 <alexdani3344@gmail.com>
2024-10-25 13:49:52 +0000

    islower

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * main - check the code.
- *
+ * void print_alphabet_x10(void); - check the code.
+ * @ch: The character to print
  * Return: Always 0.
  */
+
 void print_alphabet_x10(void)
 {
 char ch;
