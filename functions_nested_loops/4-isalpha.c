cc4021e86162efe05166e063a13543526f47f7ad
DANDEV2433 <alexdani3344@gmail.com>
2024-10-26 08:01:04 +0000

    isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isalpha - a function that checks if a character is an alphabetic letter
+ * @c: lowercase and uppercase
+ * Return: 1 if c is lowercase and uppercase.
+ */
+
+int _isalpha(int c)
+{
+if ((c >= 97 && c <= 122) || (c <= 67 && c <= 90))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+return (0);
+}
