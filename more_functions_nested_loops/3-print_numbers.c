33748b1aaa278750cfe499d7d2f2d1dfb90544d4
DANDEV2433 <alexdani3344@gmail.com>
2024-10-28 12:02:55 +0000

    print

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <unistd.h>
 #include <stdio.h>
 /**
  *  print_numbers - a function that multiplies two integers
@@ -9,10 +10,10 @@ void print_numbers(void)
 
 {
 char ch = 0;
-
-for (ch = 0; 0 <= 9; ch++)
-{
-putchar(ch);
-}
-putchar('\n');
+	while (ch <= 9)
+	{
+		write(1, &ch, 1);
+		ch++;
+	}
+	write(1, "\n", 1);
 }
