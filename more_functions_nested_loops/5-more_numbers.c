cd521c5ab9bc436820ebaf8e2316068906fff714
DANDEV2433 <alexdani3344@gmail.com>
2024-10-28 23:18:32 +0000

    x10

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * more_numbers - check the code
@@ -6,14 +7,16 @@
  */
 void more_numbers(void)
 {
-int i;
-int j;
+int i = 0, j;
 for (j = 0; j < 10; j++)
-for (i = 0; i <= 14; i++)
 {
+for (i = 0; i <= 14; i++)
 {
 _putchar(i + '0');
 }
+{
+_putchar(j + 'o');
 }
 _putchar('\n');
+}
 }
