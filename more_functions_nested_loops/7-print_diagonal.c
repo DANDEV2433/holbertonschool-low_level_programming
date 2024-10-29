3b83845040d0f3c99aec892c4f06075a0baa41af
DANDEV2433 <alexdani3344@gmail.com>
2024-10-29 14:55:48 +0000

    diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "main.h"
 /**
  * print_diagonal - check the code
@@ -9,18 +10,21 @@ void print_diagonal(int n)
 {
 int i;
 int j;
+
 if (n > 0)
-{
+
 for (i = 0; i < n ; i++)
-_putchar(' ');
-_putchar('\\');
-}
 {
-for (j = 0; j < i ; j++)
-print_diagonal(0);
-print_diagonal(2);
-print_diagonal(3);
-print_diagonal(4);
+_putchar('\\');
+_putchar('\n)
 }
+
+for (j = 0; j < i ; j++)
+{	
+_putchar(' ');
+}
+
+else
+
 _putchar('\n');
 }
