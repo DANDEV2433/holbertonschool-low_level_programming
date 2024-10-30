c6000d013a1aaa5b6438420833fd21f8404564d7
DANDEV2433 <alexdani3344@gmail.com>
2024-10-30 11:13:18 +0000

    point

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* reset_to_98 - check the code
+* @n: Write a function that takes a pointer
+* Return: void
+*/
+void reset_to_98(int *n)
+{
+*n = 98;
+}
