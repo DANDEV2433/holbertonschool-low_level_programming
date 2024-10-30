aa9141abff72b0db9285dca0d5032db6d92de2b7
DANDEV2433 <alexdani3344@gmail.com>
2024-10-30 21:42:25 +0000

    puts

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "main.h"
 /**
 * _puts - check the code
-* @s: Write a function that returns the length of a string
+* @str: Write a function that returns the length of a string
 * Return: void
 */
 void _puts(char *str)
 {
-	while (*str != '\0')
-	{
-	putchar(*str);
-	str++;
-	}
-	putchar('\n');
+for (int i = 0; str[i] != '\0'; i++)
+{
+write(1, &str[i], 1);
+}
+write(1, "\n", 1);
 }
