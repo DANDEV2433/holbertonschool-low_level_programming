ea16781103fb28ac0a48f419d29eb47ed1f6051a
DANDEV2433 <alexdani3344@gmail.com>
2024-10-31 10:55:52 +0000

    s

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,14 +8,16 @@
 */
 void print_rev(char *s)
 {
-int i = 0;
-while (s[i] != '\0')
-{
-i++;
-}
-for (i = i - 1; i >= 0; i--)
-{
-_putchar(s[i]);
-}
-_putchar('\n');
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+	i++;
+	}
+	while (i > 0)
+	{
+	i--;
+	write(1, &s[i], 1);
+	}
+	write(1, "\n", 1);
 }
