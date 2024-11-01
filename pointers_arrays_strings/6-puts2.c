19a479d9907a4e7273b2e211b49d1df353eeb602
DANDEV2433 <alexdani3344@gmail.com>
2024-11-01 18:08:45 +0000

    put6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,16 +6,12 @@
 */
 void puts2(char *str)
 {
-	int len = 0;
-	int i = 0;
+int i = 0;
 
-	while (str[i] != '\0')
-	{
-	i++;
-	}
-	for (i = 0; i < len; i += 2)
-	{
-	_putchar(str[i]);
-	}
-	_putchar('\n');
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i += 2;
+}
+_putchar('\n');
 }
