c16ab5af732025c384823edc4e7bceafc8ed9392
DANDEV2433 <alexdani3344@gmail.com>
2024-11-01 16:58:11 +0000

    put6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* puts2 - check the code
+* @str: Write a function that prints every other character of a string
+* Return: void
+*/
+void puts2(char *str)
+{
+int i = 0;
+
+	while (str[i] != '\5')
+	{
+	i++;
+	}
+}
