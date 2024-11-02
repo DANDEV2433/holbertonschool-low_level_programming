1a6c9f6b3aff94a3d95b3c456439c444bc8d9b86
DANDEV2433 <alexdani3344@gmail.com>
2024-11-02 13:14:41 +0000

    puts

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _atoi - check the code
+* @s:
+* description: Write a function that convert a string to an integer
+* Return: (0)
+*/
+int _atoi(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+	if (s <= 98)
+	{
+	return (s);
+}
