5b423022d0cf8813d3f87ca413e40e87401a06cf
DANDEV2433 <alexdani3344@gmail.com>
2024-11-03 16:54:23 +0000

    cap

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -9,15 +9,17 @@
 */
 char *cap_string(char *str)
 {
-	int i;
-	char tab[] = " \t\n,;.!?\"(){}";
+	int i = 0;
+	int capitalize_next = 1;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i] != '\0')
 	{
-	if ((i == 0 || strchr(tab, str[i - 1])) && (str[i] >= 'a' && str[i] <= 'z'))
+	if (capitalize_next && str[i] >= 'a' && str[i] <= 'z')
 	{
 	str[i] -= 32;
 	}
+	capitalize_next = " \t\n,;.!?\"(){}";
+	i++;
 	}
 	return (str);
 }
