8903001809c82fc246a20b7d8219b2cc878655f9
DANDEV2433 <alexdani3344@gmail.com>
2024-11-05 15:25:36 +0000

    recursion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -8,11 +8,13 @@
 */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-	putchar('\n');
-	return;
-	}
 	putchar(*s);
 	_puts_recursion(s + 1);
+	}
+	else if (*s == '\0')
+	{
+	putchar('\n');
+	}
 }
