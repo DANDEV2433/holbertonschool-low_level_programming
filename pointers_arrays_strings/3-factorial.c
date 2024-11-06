34450903145a415892b9190750cc5221ca7bab49
DANDEV2433 <alexdani3344@gmail.com>
2024-11-06 08:48:29 +0000

    factorial

---
+++ pointers_arrays_strings/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* factorial - check the code
+* @n: number
+* description: Write a function that returns the factorial of a given number
+* Return: 1
+*/
+int factorial(int n)
+{
+	if (n < 0)
+	return (-1);
+	else if (n > 0)
+	return ((n * factorial(n - 1)));
+	return (0);
+}
