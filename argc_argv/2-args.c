3d2d99a9872bbf3fb6a2d4db9a2a1056cac3ffc6
DANDEV2433 <alexdani3344@gmail.com>
2024-11-07 22:27:34 +0000

    argc argv

---
+++ argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - name program
+ * @argc: number of arguments int
+ * @argv: array of arguments char
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+
+	printf("%s\n", argv[i]);
+
+	return (0);
+}
