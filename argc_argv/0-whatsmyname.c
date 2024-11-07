b32efbb7711c47df340a6d344d1a12fef949b621
DANDEV2433 <alexdani3344@gmail.com>
2024-11-07 20:42:03 +0000

    argc argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - name program
+ * @argc: number of arguments int
+ * @argv: array of arguments char
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+	printf("%s\n", argv[0]);
+	}
+	return (0);
+}
