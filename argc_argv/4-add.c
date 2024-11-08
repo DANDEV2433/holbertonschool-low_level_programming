4a473fc28a8b5854ce8f3088655b35816a2d7e90
DANDEV2433 <alexdani3344@gmail.com>
2024-11-08 16:22:27 +0000

    argc argv

---
+++ argc_argv/4-add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - name program
+ * @argc: number of arguments int
+ * @argv: array of arguments char
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int result = 0;
+
+	if (argc == 1)
+	{
+	printf("0\n");
+	return (0);
+	}
