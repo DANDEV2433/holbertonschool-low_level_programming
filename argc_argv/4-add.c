15d09107c64fb4293e971e33a85e22aea5607a30
DANDEV2433 <alexdani3344@gmail.com>
2024-11-11 09:22:08 +0000

    argc

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -9,9 +9,24 @@
 int main(int argc, char *argv[])
 {
 	int result = 0;
+	int i, j;
+
 
 	if (argc == 1)
 	{
 	printf("0\n");
 	return (0);
 	}
+	if (argc == 0)
+	{
+	printf("Error\n");
+	return (1);
+	}
+	for (i = 1; i < argc; i++)
+	for (j = 0; argv[i][j] != '\0'; j++)
+	{
+	result += atoi(argv[i]);
+	printf("%d\n", result);
+	}
+	return (0);
+}
