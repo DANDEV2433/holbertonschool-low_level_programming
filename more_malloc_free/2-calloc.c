479dbe169aceb2e8a217c6320e77f3af06252455
DANDEV2433 <alexdani3344@gmail.com>
2024-11-13 20:08:07 +0000

    calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_calloc - check the code
+ * description: function that allocates memory for an array
+ * @nmemb: Number of elements in the array
+ * @size: Size of each element in bytes
+ * Return: ptr
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int 
+
+	if (nmemb == 0)
+	return (NULL);
+	if (size == 0)
+	return (NULL);
+
+	ptr[] = calloc (nmemb,sizeof(int))
+	if (ptr == NULL)
+	return (NULL);
+	for (i = 0; i < nmemb; i++)
+	{
+	ptr[i] = calloc(size,sizeof(int));
+
+
+	while ((nmemb[]) != '\0')
+	{
+	lgr1++;
+	}
+
+	while ((s2[lgr2]) != '\0')
+	{
+	lgr2++;
+	}
+
+	if (n > lgr2)
+	n = lgr2;
+
+	int *ptr[] = calloc ( ,sizeof(int));
+	if (ptr == NULL)
+	return (NULL);
+
+	for (i = 0; i < lgr1; i++)
+	dest[i] = s1[i];
+
+	for (j = 0; j < n; j++, i++)
+	dest[i] = s2[j];
+
+	dest[i] = '\0';
+
+	return (dest);
+}
