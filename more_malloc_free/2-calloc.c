7bafacc6f69ab177a99c97073ee1955e2a1592f7
DANDEV2433 <alexdani3344@gmail.com>
2024-11-14 07:38:42 +0000

    calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,19 +12,13 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *ptr;
-	unsigned int total_size, i;
 
 	if (nmemb == 0 || size == 0)
 	return (NULL);
 
-	total_size = nmemb * size;
-
-	ptr = malloc(total_size);
+	ptr = calloc(nmemb, size);
 	if (ptr == NULL)
 	return (NULL);
-
-	for (i = 0; i < total_size; i++)
-	ptr[i] = 0;
 
 	return (ptr);
 }
