c03d65d6ac95867ab33a8583a91058be86a3e1dd
DANDEV2433 <alexdani3344@gmail.com>
2024-11-19 11:38:16 +0000

    func

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -10,5 +10,21 @@
  */
 int (*get_op_func(char *s))(int, int);
 {
+	int i;
 
+	if (s != "+, -, *, /, %")
+	{
+	return (NULL);
+	}
+	if (i = 0; i < s; i++)
+
+
+	op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    }op_t;
 }
---
+++ function_pointers/3-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	num1 = atoi(a[i]);
+	num2 = atoi(b[i]);
+	calc = num1 operator num2;
+
+    struct struct op_t
+	if ( a[i] || b[i] == calc)
+	return (calc);
+    return (0);
+}
