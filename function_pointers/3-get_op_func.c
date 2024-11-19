50acf0f40e2c0924aba5b5236b4145b8f448a991
DANDEV2433 <alexdani3344@gmail.com>
2024-11-19 09:28:52 +0000

    function

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,14 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * get_op_func - correct function
+ * @s: pointer to dog
+ * @int: integer
+ * @int: integer
+ * description: function selects the correct function to perform the operation
+ * Return: 0
+ */
+int (*get_op_func(char *s))(int, int);
+{
+
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "3-calc.h"
+#include <stdio.h>
+/**
+ * op_add - sum operate
+ * @a : integer
+ * @b : integer
+ * description : returns the sum of a and b
+ * return : 0
+ */
+int op_add(int a, int b)
+{
+
+}
+/**
+ * op_sub - difference operate
+ * @a : integer
+ * @b : integer
+ * description : returns the difference of a and b
+ * return : 0
+ */
+int op_sub(int a, int b)
+{
+
+}
+/**
+ * op_mul - product operate
+ * @a : integer
+ * @b : integer
+ * description : returns the product of a and b
+ * return : 0
+ */
+int op_mul(int a, int b)
+{
+
+}
+/**
+ * op_div - division operate
+ * @a : integer
+ * @b : integer
+ * description : returns the division of a and b
+ * return : 0
+ */
+int op_div(int a, int b)
+{
+
+}
+/**
+ * op_mod - remainder division operate
+ * @a : integer
+ * @b : integer
+ * description : returns the remainder division of a and b
+ * return : 0
+ */
+int op_mod(int a, int b)
+{
+
+}
