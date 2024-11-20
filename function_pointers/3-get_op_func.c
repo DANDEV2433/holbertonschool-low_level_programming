367e3840435c745c95c39ee77c69a79c2f80ec86
DANDEV2433 <alexdani3344@gmail.com>
2024-11-20 16:18:31 +0000

    func

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,35 +1,31 @@
 #include "3-calc.h"
 #include <stddef.h>
 /**
- * get_op_func - selects the correct function to perform the operation
+ * get_op_func - selects the correct function to perform
  * @s: operator passed as argument
- * @int: integer
- * @int: integer
- * description: function selects the correct function to perform the operation
- * Return: pointer to the function that corresponds to the operator given
+ * description: selects the correct function to perform the operation
+ * Return: pointer to the function that corresponds
  */
-	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    	};
-
 int (*get_op_func(char *s))(int, int)
 {
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+};
 	int i = 0;
+
 	/* Loop to browse the array of operators */
 	while (ops[i].op != NULL)
 	{
-	/* If the current operator in the array matches the operator passed as an argument */
+	/* If the current operator == the operator passed as an argument */
 	if (*(ops[i].op) == *s)
-	{
 	/* Returns the function associated with this operator */
-	return ops[i].f;
+	return (ops[i].f);
 	/* Passes the next operator in the tab */
-	}
 	i++;
 	}
 	return (NULL);
--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 /**
  * op_add - sum operate
- * @a : integer
- * @b : integer
+ * @a: integer
+ * @b: integer
  * description : returns the sum of a and b
- * return : 0
+ * Return: the sum of a and b
  */
 int op_add(int a, int b)
 {
@@ -14,10 +14,10 @@ return (a + b);
 }
 /**
  * op_sub - difference operate
- * @a : integer
- * @b : integer
+ * @a: integer
+ * @b: integer
  * description : returns the difference of a and b
- * return : 0
+ * Return: the difference of a and b
  */
 int op_sub(int a, int b)
 {
@@ -25,10 +25,10 @@ return (a - b);
 }
 /**
  * op_mul - product operate
- * @a : integer
- * @b : integer
+ * @a: integer
+ * @b: integer
  * description : returns the product of a and b
- * return : 0
+ * Return: the product of a and b
  */
 int op_mul(int a, int b)
 {
@@ -36,10 +36,10 @@ return (a * b);
 }
 /**
  * op_div - division operate
- * @a : integer
- * @b : integer
+ * @a: integer
+ * @b: integer
  * description : returns the division of a and b
- * return : 0
+ * Return: the result of the division of a and b
  */
 int op_div(int a, int b)
 {
@@ -52,14 +52,14 @@ return (a / b);
 }
 /**
  * op_mod - remainder division operate
- * @a : integer
- * @b : integer
+ * @a: integer
+ * @b: integer
  * description : returns the remainder division of a and b
- * return : 0
+ * Return: the remainder of the division of a and b
  */
 int op_mod(int a, int b)
 {
-if (a ==0)
+if (a == 0)
 {
 	printf("Error\n");
 	exit(100);
