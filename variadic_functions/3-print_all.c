5cbd0f5590aa7bc2551eb629363eddc3db27dfe5
DANDEV2433 <alexdani3344@gmail.com>
2024-11-21 18:32:36 +0000

    print all

---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_all - prints anything
+ * @format: list of types of arguments
+ * @*: 
+ * description: function that prints anything
+ * Return: 
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	char *str;
+
+	if (format != ap)
+	printf(" "'\n')
+
+	if (str == NULL)
+	str = "(nil)";
+	va_start(ap, format);
+       	while (i = 0, i < format)
+	i++;
+	{
+	str = va_arg(ap, char*);
+	va_end(ap);
+	printf("\n");
