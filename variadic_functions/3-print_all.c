c1e95291a42ea464195431ec6e41175eba2a3658
DANDEV2433 <alexdani3344@gmail.com>
2024-11-21 21:01:54 +0000

    print all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -12,16 +12,36 @@ void print_all(const char * const format, ...)
 {
 	va_list ap;
 	char *str;
+	unsigned int i;
+	int len;
+	char *separator;
 
-	if (format != ap)
-	printf(" "'\n')
-
-	if (str == NULL)
-	str = "(nil)";
 	va_start(ap, format);
-       	while (i = 0, i < format)
+	while (i = 0, i < format)
 	i++;
+	while (len = 0, len < format)
+	len++;
+	
+	if (separator != NULL && i < n - 1)
+	{
+	printf("%s", separator);
+	}
+	if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
+	{
+	if (format[i] == 'c')
+	printf("%c", va_arg(ap, int));
+	else if (format[i] == 'i')
+	printf("%d", va_arg(ap, int));
+	else if (format[i] == 'f')
+	printf("%f", va_arg(ap, double));
+	else if (format[i] == 's')
 	{
 	str = va_arg(ap, char*);
+	if (str == NULL)
+        str = "(nil)";
+	printf("%s", str);
+	}
 	va_end(ap);
 	printf("\n");
+	}
+}
