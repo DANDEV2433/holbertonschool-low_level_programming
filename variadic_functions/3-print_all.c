d289bc2bcb895e30505c92cc66e84d0d552e9fda
DANDEV2433 <alexdani3344@gmail.com>
2024-11-22 11:06:19 +0000

    function

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,43 +3,37 @@
 #include <stdio.h>
 /**
  * print_all - prints anything
- * @format: list of types of arguments
- * @*: 
+ * @format: string of caracter
+ * @char *: pointer to const char 
  * description: function that prints anything
  * Return: 
  */
 void print_all(const char * const format, ...)
 {
 	va_list ap;
-	char *str;
-	unsigned int i;
-	int len;
-	char *separator;
+	char *s;
+	int i= 0;
+	char separator=",";
 
 	va_start(ap, format);
-	while (i = 0, i < format)
-	i++;
-	while (len = 0, len < format)
-	len++;
+
+	if (format != NULL)
+	{
+		while (format[i] != '\0')
+		{
+		i++;
+		}
 	
-	if (separator != NULL && i < n - 1)
-	{
-	printf("%s", separator);
-	}
-	if (format[i] == 'c' || format[i] == 'i' || format[i] == 'f' || format[i] == 's')
-	{
+	if (separator != NULL && format[i] - 1)
+		printf("%s", separator);
 	if (format[i] == 'c')
-	printf("%c", va_arg(ap, int));
+		printf("%c", va_arg(ap, char));
 	else if (format[i] == 'i')
-	printf("%d", va_arg(ap, int));
+		printf("%d", va_arg(ap, int));
 	else if (format[i] == 'f')
-	printf("%f", va_arg(ap, double));
+		printf("%f", va_arg(ap, double));
 	else if (format[i] == 's')
-	{
-	str = va_arg(ap, char*);
-	if (str == NULL)
-        str = "(nil)";
-	printf("%s", str);
+		printf("%c", va_arg(ap, char*));
 	}
 	va_end(ap);
 	printf("\n");
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,23 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+#include <stdio.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct print_element -
+ *
+ *
+ */
+typedef struct print_element
+{
+	char type;
+	void (*f)(va_list ap, char *separator);
+}print_t;
 
 #endif
