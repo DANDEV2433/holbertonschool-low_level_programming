c1bcd686feb2d2ccae4cfc7456a11b50c58de77a
DANDEV2433 <alexdani3344@gmail.com>
2024-12-04 11:03:19 +0100

    print list

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -11,12 +11,12 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t count = 0;
 
+	if (h == NULL)
+	return (0);
+
 	while (h != NULL)
 	{
-	if (h->str == NULL)
-	printf("[0] (nil)\n");
-	else
-	printf("[%u] %s\n", h->len, h->str);
+	printf("[%u]", h->n);
 	h = h->next;
 	count++;
 	}
